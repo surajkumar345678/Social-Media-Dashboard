e23dec286d569f4d5993d5e5987f507b6a8c6bb0
surajkumar345678 <kumarsuraj345678@gmail.com>
2025-01-30 17:45:10 +0530

    Refactor SCSS structure and enhance layout with new header and toggle components

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,10 @@ const babel = require('gulp-babel');
 const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
 
-// Use dart-sass for @use
-//sass.compiler = require('dart-sass');
-
 // Sass Task
 function scssTask() {
   return src('app/scss/style.scss', { sourcemaps: true })
-    .pipe(sass())
+    .pipe(sass().on('error', sass.logError)) // Prevent errors from stopping Gulp
     .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(dest('dist', { sourcemaps: '.' }));
 }
@@ -50,10 +47,7 @@ function browserSyncReload(cb) {
 // Watch Task
 function watchTask() {
   watch('*.html', browserSyncReload);
-  watch(
-    ['app/scss/**/*.scss', 'app/**/*.js'],
-    series(scssTask, jsTask, browserSyncReload)
-  );
+  watch(['app/scss/**/*.scss', 'app/js/**/*.js'], series(scssTask, jsTask, browserSyncReload));
 }
 
 // Default Gulp Task
