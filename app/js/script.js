0b9fd3f9299472acdf51081cc8acfc9aaff7fa66
surajkumar345678 <kumarsuraj345678@gmail.com>
2025-02-05 12:55:37 +0530

    Implement dark/light mode toggle functionality and update card styles for improved responsiveness

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,9 @@
-console.log('Hello, world!');
+const radioButtons = document.querySelectorAll(".toggle__wrapper input");
+for (let i = 0; i < radioButtons.length; i++) {
+    radioButtons[i].addEventListener('click', event => {
+        document.getElementById("dark").checked
+          ? (document.querySelector("body").classList = "dark")
+          : (document.querySelector("body").classList = "light");
+    })
+}
+
